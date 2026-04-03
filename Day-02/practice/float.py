f12afae294d493ab4f5a8302b936138459fd2a70
BODA MOHAN REDDY <bodhamohanreddy05@gmail.com>
2026-04-03 15:42:01 +0000

    added practice files for day 2

---
+++ Day-02/practice/float.py
@@ -0,0 +1,23 @@
+a = 55
+b = 20
+
+add = a + b
+print("Addition:", add)
+
+sub = a - b
+print("Subtraction:", sub)
+
+mul = a * b
+print("Multiplication:", mul)
+
+div = a / b
+print("Division:", div)
+
+mod = a % b
+print("Modulus:", mod)
+
+div_int = a // b
+print("Integer Division:", div_int)
+
+abs = abs(-966)
+print("Absolute Value:", abs)
---
+++ Day-02/practice/test.py
@@ -0,0 +1,12 @@
+fn = 'M o h a n'
+ln = "Reddy"    
+name = "  mohan reddy bodha  "
+
+print(fn)
+print(fn + ' ' + ln)  # Output: Mohan reddy
+print("The length of strings", len(fn), "and",len(ln))  # Output: 5 5
+print(fn.upper())
+print(ln.lower())
+print(fn.replace(' ', ''))
+print(name.strip())
+print(name.split())
